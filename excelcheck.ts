c44dbcc27215610e116e0132dd86604e94594654
Shanmuga dharshini <dharshini@energetics.ai>
2025-02-10 17:19:49 +0530

    fixed

--- excelcheck.ts
+++ excelcheck.ts
@@ -1,19 +1,44 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import multer from 'multer'
 import xlsx from 'xlsx'
+import mysql from 'mysql2/promise'
+import pool from './db'
+
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
-app.post('/chats', upload.single('file'), async (req: Request, res: Response): Promise<void> => {
+app.post('/chats', upload.single('file'), async (req: any, res: Response): Promise<void> => {
     if (!req.file) {
         res.status(400).json({ error: "no file" })
     }
     try {
 
-        // const workbook = xlsx.readFile(req.file.path)
+        const workbook = xlsx.readFile(req.file.path);
+        const sheetname = workbook.SheetNames[0];
+        const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheetname]);
+
+        const chat_data = data.map((row: any) => ([
+            row.sender,
+            row.receiver,
+            row.Message,
+            new Date(row.Timestamp)
+        ]));
+
+        const query = 'INSERT INTO chat_history(sender, receiver, message,timestamp) VALUES ?';
+
+        const connection = await pool.getConnection();
+        await connection.query(query, [chat_data]);
+
+        connection.release();
+        res.json({ message: "chat history imported succesffuly" });
     } catch (error) {
         console.error(error, "error");
         res.status(500).json({ error: "error occured" });
     }
+});
+
+app.listen(3000, () => {
+    console.log("server running on port");
+
 });
